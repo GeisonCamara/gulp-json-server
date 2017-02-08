2209947d32b0c8b32b7142f1803a9227c6a559b1
GeisonCamara <geison.camara@hotmail.com>
2017-02-08 21:26:12 -0200

    Criar o servidor

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+﻿var gulp = require('gulp');
+var jsonServer = require('gulp-json-srv');
+
+var server = jsonServer.create();
+
+gulp.task('srv', function(){
+	return gulp.src("product.json")
+		.pipe(server.pipe());
+});
+
+gulp.task('default', function () {
+	gulp.start('srv');
+});
